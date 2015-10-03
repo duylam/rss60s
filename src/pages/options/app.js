8efee074dcaa612116b9963412bb7e0cfb5103d7
duylam <duylamphuong@gmail.com>
2015-10-03 22:44:04 +0700

    implement toggle for editing rss entries in option page

--- src/pages/options/app.js
+++ src/pages/options/app.js
@@ -1,4 +1,35 @@
 angular.module("App", [])
+  .directive("void", function() {
+    return {
+      restrict: "A",
+      compile: function(element) {
+        element.attr("href", "javascript:void(0);"); 
+      }
+    };
+  })
   .controller("MainCtrl", function($scope) {
-    $scope.title = "Option page";
+    $scope.appearance = {
+      newSourceNamePlaceholderText: chrome.i18n.getMessage('news_newSourceNamePlaceholderText'),
+      newSourceUrlPlaceholderText: chrome.i18n.getMessage('news_newSourceUrlPlaceholderText'),
+      newSourceSectionText: chrome.i18n.getMessage('news_newSourceSectionText')
+    };
+
+    $scope.updateEntry = function(entry) {
+      entry.edit = false;
+    };
+
+    $scope.entries = [
+      {
+        edit: false,
+        id: 1,
+        name: 'test',
+        url: 'http://local1.com'
+      },
+      {
+        edit: false,
+        id: 2,
+        name: 'test2',
+        url: 'http://local2.com'
+      }
+    ];
   });
